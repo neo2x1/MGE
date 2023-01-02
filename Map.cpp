fbed38bdbd6218c517d5619b5067741fce2798a0
neo <michal.pawlo20069@wp.pl>
2023-01-02 18:49:03 +0100

    map upgrade

--- Map.cpp
+++ Map.cpp
@@ -1,1 +1,24 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "Map.h"
+Map::Map() {
+	for (int i = 0; i < 50; i++) {
+		for (int j = 0; j < 100; j++) board[i][j] = '#';
+	}
+	for (int i = 1; i < 49; i++) {
+		for (int j = 1; j < 99; j++) board[i][j] = ' ';
+	}
+	for (int i = 1; i < 49; i++) {
+		for (int j = 1; j < 49; j++) {
+
+		 }
+	}
+}
+void Map::viewMap() {
+	for (int i = 0; i < 50; i++) {
+		for (int j = 0; j < 100; j++) std::cout << board[i][j];
+		std::cout << std::endl;
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include "Map.h"
+
 #include <iostream>
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
-	std::cin.get();
+	Map map;
+	map.viewMap();
 	return 0;
 }
